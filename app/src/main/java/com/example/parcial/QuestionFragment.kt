f2e2a2acfc2b7b9d323f55f194f73a89e6fd217e
Juan Alexis Quispe Luque <jquispel@ulasalle.edu.pe>
2024-10-20 17:31:13 -0500

    Hice un breve cambio en el layout de question, ya que vi una incoherencia y declaramos las variables en QuestionFragment.

--- app/src/main/java/com/example/parcial/QuestionFragment.kt
+++ app/src/main/java/com/example/parcial/QuestionFragment.kt
@@ -1,14 +1,28 @@
 package com.example.parcial
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.fragment.findNavController
+import com.example.parcial.Question
 
 class QuestionFragment : Fragment() {
+    private lateinit var questions: List<Question>
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    private val maxQuestions = 10
+    private var timer: CountDownTimer? = null
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+    }
 }
